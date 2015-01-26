a24b08e11650695aa65df60ec1224b714a266976
saichaitanya88 <saichaitanya88@gmail.com>
2015-01-25 21:35:57 -0800

    added SirenState

--- SecureBox.rb
+++ SecureBox.rb
@@ -58,6 +58,7 @@ class SecureBox
 		@invalid_keypass_counter = 0
 		Logger.print("Initialized.", Logger.info)
 		Logger.print("Current State is #{current_state}", Logger.details)
+		beep(400)
 	end
 
 	def current_state
@@ -68,26 +69,31 @@ class SecureBox
 	def process_event(event)
 		# processes the event by picking the appropriate path based on the starting state, event and sets the current_state to end_state from the selected path
 		Logger.print("Process_Event - #{events[event]}(#{event}) with Current_State - #{current_state}(#{events.key(current_state)})", Logger.log)
-		path = @@state_paths.find {|p|p.event == event && p.starting_state == events.key(current_state)}
+		path = @@state_paths.find { |p| p.event == event && p.starting_state == events.key(current_state) }
 		if (path == nil)
 			Logger.print("No path found for #{events[event]}(#{event}) with Current_State - #{current_state}(#{events.key(current_state)})", Logger.warning)
 			return nil
 		end
+		previous_state = @current_state
 		@current_state = events[path.end_state]
-		initiate_trigger(event, @current_state)
+		initiate_trigger(event, @current_state, previous_state)
 		Logger.print("Current State is #{@current_state}", Logger.details)
 	end
 
-	def initiate_trigger(event, trigger_state)
+	def initiate_trigger(event, trigger_state, previous_state)
 		# initiates triggers for each specific event. 
 		# custom code goes here
 		if event == events.key('DOOR_OPEN') and events.key(trigger_state) == events.key('ARMED')
+			beep(500)
 			Logger.print("Event - #{events[event]}", Logger.log)
 			Thread.new { trigger_door_open }
 		elsif event == events.key('KEYPASS_FAIL') and events.key(trigger_state) == events.key('DISARMED')
-			beep(300)
+			beep(500)
 			Logger.print("Event - #{events[event]}", Logger.log)
 			Thread.new { trigger_keypass_fail_in_disarmed_state }
+		elsif event == events.key('KEYPASS_FAIL') and events.key(previous_state) == events.key("ARMED")
+			Logger.print("Event - #{events[event]}", Logger.log)
+			Thread.new { trigger_siren_mode }
 		end
 	end
 
@@ -98,6 +104,7 @@ class SecureBox
 			@invalid_keypass_counter = @invalid_keypass_counter + 1
 			Logger.print("Incorrect Keycode - Attempt #{@invalid_keypass_counter.to_s}", Logger.warning)
 			if (@invalid_keypass_counter > 5)
+				beep(300)
 				@current_state = events[siren]
 				Logger.print("Current State is #{@current_state}", Logger.details)
 			end
@@ -126,7 +133,6 @@ class SecureBox
 			end
 			pulse_counter = pulse_counter + 1
 			if (pulse_counter % 20 == 0)
-				beep(siren)
 				Logger.print("#{current_state}(#{events.key(current_state)})", Logger.info)
 			end
 		end
@@ -136,6 +142,20 @@ class SecureBox
 		end
 		Logger.print("Stopping Thread: trigger_door_open", Logger.info)
 		Logger.print("Current State is #{@current_state}", Logger.details)
+		Thread.stop
+	end
+
+	def trigger_siren_mode
+		Logger.print("Starting Thread: trigger_siren_mode", Logger.info)
+		while (true) do
+			if (events.key(current_state) == events.key("SIREN"))
+				beep(300)
+				sleep(5)
+			else
+				break
+			end
+		end
+		Logger.print("Stopping Thread: trigger_siren_mode", Logger.info)
 		Thread.stop
 	end
 
